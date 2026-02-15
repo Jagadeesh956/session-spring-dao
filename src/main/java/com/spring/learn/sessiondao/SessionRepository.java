1621ccc3fe5281d7a214d4c33da0b6deb3dc5295
Jagadeesh <jagadeeshporalla2@gmail.com>
2026-02-14 19:51:42 -0700

    fixed updateTime Query

--- src/main/java/com/spring/learn/sessiondao/SessionRepository.java
+++ src/main/java/com/spring/learn/sessiondao/SessionRepository.java
@@ -1,4 +1,5 @@
 package com.spring.learn.sessiondao;
+
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -10,17 +11,24 @@ import java.util.UUID;
 
 public interface SessionRepository extends JpaRepository<Session, UUID> {
     @Transactional
-     @Modifying
-     @Query("update Session s set s.Status='INACTIVE' where s.sessionId=:id")
-   void updateStatus(@Param("id") UUID id);
+    @Modifying
+    @Query("update Session s set s.Status='INACTIVE' where s.sessionId=:id")
+    void updateStatus(@Param("id") UUID id);
 
     Long l = System.currentTimeMillis();
+
     @Transactional
     @Modifying
-    @Query("update Session s set s.LastActive=:time")
+    @Query("update Session s set s.LastActive=:time where s.userID=:id")
     void updateTime(@Param("id") UUID id, @Param("time") Long time);
-    @Query(value = "select * from Session s where s.user_id=:id",nativeQuery = true)
+
+    @Query(value = "select * from Session s where s.user_id=:id", nativeQuery = true)
     List<Session> userSessions(@Param("id") long Id);
+
+    @Modifying
+    @Transactional
+    @Query("delete from Session s where s.LastActive<:expiry")
+    int removeSessions(@Param("expiry") long LastActive);
 }
 
 
